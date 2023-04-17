8f38d8cbb1b9d0c15add45622a660b5b6900be2e
lat9723 <lauraagustinatrotta@gmail.com>
2023-04-17 17:56:59 -0300

    creating movie recommender

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import { TextField, Button, Typography } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#2196f3',
+      dark: '#0d8aed',
+    },
+  },
+});
+
+const Wrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100vh',
+});
+
+const Title = styled(Typography)({
+  fontSize: '2rem',
+  fontWeight: 'bold',
+  marginBottom: '1rem',
+});
+
+const Form = styled('form')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const Label = styled(Typography)({
+  fontSize: '1.2rem',
+  fontWeight: 'bold',
+  marginBottom: '0.5rem',
+});
+
+const Input = styled(TextField)({
+  marginBottom: '1rem',
+  border: '1px solid #111',
+});
+
+const SubmitButton = styled(Button)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  color: 'white',
+  fontSize: '1.2rem',
+  fontWeight: 'bold',
+  marginTop: '1rem',
+  '&:hover': {
+    backgroundColor: theme.palette.primary.dark,
+  },
+}));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <Title variant="h1">Movie Recommender</Title>
+        <Form>
+          <Label htmlFor="director-name">Name of Movie's director that you like:</Label>
+          <Input id="director-name" name="director-name" required />
+          <SubmitButton type="submit">Submit</SubmitButton>
+        </Form>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,49 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#2196f3',
+      dark: '#0d8aed',
+      contrastText: '#ffffff',
+    },
+    background: {
+      default: '#111',
+      paper: '#333',
+    },
+    text: {
+      primary: '#fff',
+      secondary: '#bdbdbd',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+    h1: {
+      fontSize: '3rem',
+      fontWeight: 'bold',
+    },
+    h2: {
+      fontSize: '2rem',
+      fontWeight: 'bold',
+    },
+    h3: {
+      fontSize: '1.5rem',
+      fontWeight: 'bold',
+    },
+    h4: {
+      fontSize: '1.2rem',
+      fontWeight: 'bold',
+    },
+    body1: {
+      fontSize: '1rem',
+      lineHeight: 1.5,
+    },
+    button: {
+      textTransform: 'uppercase',
+      fontWeight: 'bold',
+    },
+  },
+});
+
+export default theme;
