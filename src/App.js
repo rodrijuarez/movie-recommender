6e311ea102180929c6c0a081215f8ee71d78414e
lat9723 <lauraagustinatrotta@gmail.com>
2023-04-19 22:56:44 -0300

    connecting front with back stored in https://github.com/rodrijuarez/movie-recommender-backend

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import theme from './theme';
 import { ThemeProvider, styled } from '@mui/material/styles';
 import { TextField, Button, Typography } from '@mui/material';
+import axios from 'axios';
 
 const Wrapper = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -27,7 +28,7 @@ const Form = styled('form')(({ theme }) => ({
   justifyContent: 'center',
 }));
 
-const Label = styled(Typography)(({ theme }) => ({
+const Label = styled('label')(({ theme }) => ({
   fontSize: '1.2rem',
   fontWeight: 'bold',
   marginBottom: '0.5rem',
@@ -49,19 +50,63 @@ const SubmitButton = styled(Button)(({ theme }) => ({
   },
 }));
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Wrapper>
-        <Title variant="h1">Movie Recommender</Title>
-        <Form>
-          <Label htmlFor="director-name">Name of Movie's director that you like:</Label>
-          <Input id="director-name" name="director-name" variant="outlined" required />
-          <SubmitButton type="submit">Submit</SubmitButton>
-        </Form>
-      </Wrapper>
-    </ThemeProvider>
-  );
+export class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('myRequest: ' + this.state.value);
+  
+    axios.get('http://localhost:3001/recommendations', {
+      params: {
+        director: this.state.value
+  //       curl --location --request GET "localhost:3000/recommendations?query=Linklater" \
+  // --header "Content-Type: application/json"
+      }
+    })
+    .then(response => {
+      console.log(response.data);
+      console.log(response.text);
+      // Do something with the response data, such as update the state of the component
+    })
+    .catch(error => {
+      console.error(error);
+      // Handle any errors that occurred during the API call
+    });
+  }
+
+  render() { 
+
+    return (
+      <ThemeProvider theme={theme}>
+        <Wrapper>
+          <Title variant="h1">Movie Recommender</Title>
+          <Form onSubmit={this.handleSubmit}>
+            <Label htmlFor="director-name">Name of Movie's director that you like:</Label>
+            <Input
+              id="director-name"
+              name="director-name"
+              variant="outlined"
+              required
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <SubmitButton type="submit">Submit</SubmitButton>
+          </Form>
+        </Wrapper>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
